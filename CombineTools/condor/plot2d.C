36b09a114acba45722afa01cfc6d2676d3d6138a
pallabidas <pallabi91@gmail.com>
2026-05-05 18:04:18 +0200

    FR comments

--- CombineTools/condor/plot2d.C
+++ CombineTools/condor/plot2d.C
@@ -5,40 +5,42 @@
 
 void plot2d(string ch)
 {
-
+  gStyle->SetPalette(kBeach);
+  gStyle->SetPaintTextFormat("4.0f");
+  gStyle->SetPadTickX(1);
+  gStyle->SetPadTickY(1);
   TCanvas *c1 = new TCanvas("c1","c1",0,0,600,600);
   c1->SetFillColor(0);
   c1->GetFrame()->SetFillColor(0);
   c1->GetFrame()->SetBorderSize(6);
   c1->GetFrame()->SetBorderMode(-1);
   c1->SetRightMargin(0.2);
-  gStyle->SetPalette(kBeach);
-  gStyle->SetPaintTextFormat("4.1f");
+  c1->SetLeftMargin(0.11);
+  c1->SetBottomMargin(0.11);
 
   float m1values[6] = {12.5, 17.5, 25, 35, 45, 55};
   float m2values[11] = {15, 25, 35, 45, 55, 65, 75, 85, 95, 105, 115};
-  TH2F   *hist       = new TH2F("", "", 5, m1values, 10, m2values);
+  TH2F *hist = new TH2F("", "", 5, m1values, 10, m2values);
 
   double x;
   double y;
   double z;
 
-
   hist->GetXaxis()->SetTitle("m_{#phi_{1}} [GeV]");
-  hist->GetXaxis()->SetTitleSize(0.04);
+  hist->GetXaxis()->SetTitleSize(0.048);
   hist->GetXaxis()->CenterTitle();
-  hist->GetXaxis()->SetTitleOffset(1.1);
+  hist->GetXaxis()->SetTitleOffset(0.95);
 
   hist->GetYaxis()->SetTitle("m_{#phi_{2}} [GeV]");
-  hist->GetYaxis()->SetTitleSize(0.04);
+  hist->GetYaxis()->SetTitleSize(0.048);
   hist->GetYaxis()->CenterTitle();
-  hist->GetYaxis()->SetTitleOffset(1.15);
+  hist->GetYaxis()->SetTitleOffset(1.);
 
   //hist->GetZaxis()->SetTitle("95% CL upper limit on #sigma(H#rightarrow #phi_{1} #phi_{2} #rightarrow2#tau4b/2#tau2b) (pb)");
-  hist->GetZaxis()->SetTitle("#sigma_{SM}B(H#rightarrow #phi_{1} #phi_{2} #rightarrow2#tau4b,2#tau2b) [pb]");
+  hist->GetZaxis()->SetTitle("#sigma_{SM}B(H #rightarrow #phi_{1} #phi_{2} #rightarrow 2#tau4b,2#tau2b) [pb]");
   hist->GetZaxis()->CenterTitle();  // Optional: Center the title
   hist->GetZaxis()->SetTitleOffset(1.4);  // Adjust to push the title to the right
-  hist->GetZaxis()->SetTitleSize(0.04);
+  hist->GetZaxis()->SetTitleSize(0.048);
   hist->GetZaxis()->SetLabelSize(0.035);
   hist->SetMaximum(100.);
   if (ch == "allchannels") { hist->SetMaximum(60.); }
@@ -58,22 +60,27 @@ void plot2d(string ch)
   text->SetTextSize(0.04);
   text->SetTextFont(42);
 
+  TLatex* text2 = new TLatex(0.11, 0.91, "#bf{CMS}");
+  text2->SetNDC();
+  text2->SetTextSize(0.042);
+  text2->SetTextFont(42);
+
   string channel;
   if (ch == "mutau") { channel = "#mu#tau_{h}"; }
   else if (ch == "etau") { channel = "e#tau_{h}"; }
   else if (ch == "emu") { channel = "e#mu"; }
   else if (ch == "allchannels") { channel = "Combined"; }
-  TPaveText* pave = new TPaveText(0.6,0.73,0.78,0.88,"NDC");
+  TPaveText* pave = new TPaveText(0.57,0.69,0.78,0.82,"NDC");
   pave->SetFillColor(0);
   pave->SetBorderSize(0);
   pave->SetTextSize(0.042);
   pave->SetTextFont(42);
   pave->SetTextAlign(12);
-  pave->AddText("#bf{CMS}");
+  //pave->AddText("#bf{CMS}");
   pave->AddText(channel.c_str());
   pave->AddText("Cut-based");
 
-  TPaveText* pave2 = new TPaveText(0.2,0.12,0.78,0.178,"NDC");
+  TPaveText* pave2 = new TPaveText(0.23,0.84,0.75,0.87,"NDC");
   pave2->SetFillColor(0);
   pave2->SetBorderSize(0);
   pave2->SetTextSize(0.042);
@@ -90,6 +97,7 @@ void plot2d(string ch)
   pave->Draw("same");
   pave2->Draw("same");
   text->Draw("same");
+  text2->Draw("same");
   c1->RedrawAxis();
   std::string title = "plotLimit_2d_"+ch;
   c1->SaveAs((title+".png").c_str(),"png");
--- CombineTools/condor/plotLimits.py
+++ CombineTools/condor/plotLimits.py
@@ -1,6 +1,6 @@
 import os
 
-plotdir = "/eos/user/p/pdas/www/Ha1a2/limits/010426/"
+plotdir = "/eos/user/p/pdas/www/Ha1a2/dataMC/PAPER/v14/limits/"
 
 # false for expected limits only, true for expected and observed
 showObserved = "true"
